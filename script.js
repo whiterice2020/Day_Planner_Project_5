d764d7b77e3a09ea8a001c86d27a57e589413958
Paul DelPadre <PDelPadre@rice.local>
2020-06-19 21:22:05 -0400

    Updating Code

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 // vars
 var tasks = [];
 var workHours =[8,9,10,11,12,1,2,3,4,5];
+var currentTime = 
 
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// ✓ The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
-// ✓ The current day is displayed at the top of the calendar.
-// *******moment.js***************
 console.log(tasks);
 
 
@@ -25,15 +21,17 @@ function init() {
 
     tasks = storedTasks; 
   }
-
-  task.forEach(function(task) {
-    // find the element with the id  of 'description-" + task id"
-
-    //update its value with task.description
-  });
-
-  //Go through each task. get the id from the task, update that description box with the task.
 }
+//   task.forEach(function(task) {
+//     // find the element with the id  of 'description-" + task id"
+
+//     //update its value with task.description
+//   });
+
+//   //Go through each task. get the id from the task, update that description box with the task.
+// }
+
+
 // Saving Taks to Local Storage
 function storedTasks() {
     // Stringify and set "tasks" key in localStorage to tasks array
@@ -46,33 +44,17 @@ function storedTasks() {
 $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 moment().format("YYYY-MM-DD HH:mm");
 
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// **********bootstrap rows and columns********************
+// 12 hour current time
 
 
-// WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // ✓ Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
-// ?????
 
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// ✓ Each timeblock contains an input field and save button.
-// on click event
-// input field
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// ✓ Clicking a time block's save button saves the input text to local storage, 
-// allowing the text to persist when the application is refreshed.
-// Local Storage
+// RowColorUpdate
 
 
 
+// Click event for save button which saves to local storage
 $(".saveMe").on("click", function(event) {
     var description = $(this).prev()[0].value;
     var hour = $(this).attr('id');
